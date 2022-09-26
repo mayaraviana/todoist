6c10b9777d5a20d9387ed3869f2bff23339220dc
mayaraviana <mayaraviana60@hotmail.com>
2022-09-26 11:18:49 -0300

    Change index tittle, remove unused css, add uuid

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  function consoleLogTaks() {
+    console.log(tasks);
+  }
+  const [tasks, setTask] = useState([
+    {
+      id: uuidv4(),
+      name: "Essa é uma tarefa teste",
+    },
+  ]);
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <h1>Todo List</h1>
+      <button onClick={consoleLogTaks}>Console Log taks</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
